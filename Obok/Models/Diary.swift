60b3325feccb08144eaefdcfdef1ee2aff2dbcc8
min-gyoung <alsrud0205@naver.com>
2024-11-23 22:54:11 +0900

    project setting, model def

--- Obok/Models/Diary.swift
+++ Obok/Models/Diary.swift
@@ -5,3 +5,17 @@
 //  Created by 김민경 on 11/23/24.
 //
 
+// 일기 데이터를 정의
+
+import Foundation
+
+struct Diary: Identifiable {
+    let id: UUID // 고유 식별자
+    var date: Date // 일기 작성 날짜
+    var title: String // 일기 제목
+    var content: String // 일기 내용
+    var subject: String // 과목
+    var understandingLevel: Int // 이해도 (1~10)
+    var distractions: [String] // 공부 방해 요인
+    var improvements: [String] // 아쉬운 점
+}
