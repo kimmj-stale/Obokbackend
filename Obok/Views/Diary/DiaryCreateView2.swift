e3519d66f89bee48b540f9df6c7e010273283593
min-gyoung <alsrud0205@naver.com>
2024-12-24 16:12:21 +0900

    understand percent

--- Obok/Views/Diary/DiaryCreateView2.swift
+++ Obok/Views/Diary/DiaryCreateView2.swift
@@ -5,13 +5,112 @@
 //  Created by 김민경 on 12/24/24.
 //
 
+// 새 일기 생성 - 단계별 템플릿 제공 - 2
+
 import SwiftUI
 
 struct DiaryCreateView2: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var currentPage = 1
+    private let totalPages = 5
+    @State private var understand: String = ""
+
+
     var body: some View {
-        Text("DiaryCreateView2")
-            .font(.system(size: 24))
-            .fontWeight(.bold)
-            .foregroundColor(.black)
+        ZStack {
+            VStack(alignment: .leading) {
+                // 상단 네비게이션 영역
+                HStack {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss() // 이전 화면으로 돌아가기
+                    }) {
+                        Image("backbutton")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                    }
+                    
+                    Spacer()
+                    
+                    Text("학습일기 작성하기")
+                        .font(.system(size: 18))
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Spacer()
+                    
+                    Text("\(currentPage) / \(totalPages)")
+                        .font(.system(size: 16))
+                        .foregroundColor(.black)
+                }
+                .padding([.top, .horizontal], 24)
+                
+                // 학습일기 작성하기 상단 바
+                ZStack(alignment: .leading) {
+                    // 전체 길이
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color.gray.opacity(0.2))
+                        .frame(height: 10)
+                    
+                    // 현재 진행된 부분
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(CustomColor.colors.first!) // 진행중인 부분 색상
+                        .frame(width: CGFloat(currentPage) / CGFloat(totalPages) * (UIScreen.main.bounds.width - 48), height: 10)
+                }
+                .padding(.horizontal, 24)
+                .padding(.top, 11)
+                
+                Spacer().frame(height: 84)
+                
+                // 페이지 입력 영역
+                HStack {
+                    Text("오늘 공부는")
+                        .font(.system(size: 15))
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                        .frame(width: 70, height: 30)
+                        .overlay(
+                            TextField("", text: $understand)
+                                .keyboardType(.numberPad)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 16))
+                        )
+                    Text("% 잘 이해했다고 생각해요")
+                        .font(.system(size: 15))
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                }
+                .padding(.horizontal, 25)
+                .padding(.bottom, 320)
+            }
+            Spacer()
+                
+                
+                
+                // 다음으로 버튼
+                NavigationLink(destination: DiaryCreateView2()) {
+                    Text("다음으로")
+                        .font(.system(size: 15))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, minHeight: 48)
+                        .background(Color.black)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 25)
+                        .padding(.bottom, 20)
+                }
+            }
+            .navigationBarHidden(true)
+        }
+    }
+
+
+
+struct DiaryCreateView2_Previews: PreviewProvider {
+    static var previews: some View {
+        DiaryCreateView2()
     }
 }
