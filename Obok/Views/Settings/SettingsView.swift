bd216cefb79b0e374c0e9714d182614cb0269f7d
min-gyoung <alsrud0205@naver.com>
2024-12-28 19:16:39 +0900

    setting alarm

--- Obok/Views/Settings/SettingsView.swift
+++ Obok/Views/Settings/SettingsView.swift
@@ -2,15 +2,61 @@
 //  SettingsView.swift
 //  Obok
 //
-//  Created by 김민경 on 11/23/24.
+//  Created by 김민경 on 11/28/24.
 //
 
 import SwiftUI
 
 struct SettingsView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var isNotificationOn: Bool = false
+    
     var body: some View {
-        Text("설정 화면")
-            .font(.largeTitle)
+        VStack(spacing: 20) {
+            HStack {
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss() // 이전 화면으로 돌아가기
+                }) {
+                    Image("back")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                }
+                Text("설정")
+                    .font(.system(size: 16))
+                    .fontWeight(.bold)
+                    .padding(.leading)
+                Spacer()
+            }
+            
+            // 알림 섹션
+            VStack(alignment: .leading, spacing: 10) {
+                HStack {
+                    Image("alarm")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+
+                    Text("알림")
+                        .font(.system(size: 15))
+                    Spacer()
+                    Toggle("", isOn: $isNotificationOn)
+                        .labelsHidden()
+                }
+                Text("매일 저녁 21시, 23시에 상담바 알림을 받아요")
+                    .font(.subheadline)
+                    .foregroundColor(.red)
+                    .padding(.leading, 30)
+            }
             .padding()
+        }
+        .padding()
+        .background(Color.white)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
     }
 }
