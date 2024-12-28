8e3729098adbffc4ffb3bd8536d832c976dc4f10
min-gyoung <alsrud0205@naver.com>
2024-12-28 16:53:00 +0900

    login page hex color

--- Obok/Views/Authentication/LoginView.swift
+++ Obok/Views/Authentication/LoginView.swift
@@ -35,7 +35,7 @@ struct LoginView: View {
                 }
                 .padding()
                 .frame(maxWidth: .infinity)
-                .background(Color.yellow)
+                .background(Color(hex: "F9DB00"))
                 .cornerRadius(30)
             }
             .padding(.horizontal)
@@ -68,6 +68,23 @@ struct LoginView: View {
             Spacer()
         }
         .background(Color.white)
+    }
+}
+
+// Color 확장 함수
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.currentIndex = hex.startIndex
+        
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        
+        let red = Double((rgb >> 16) & 0xFF) / 255.0
+        let green = Double((rgb >> 8) & 0xFF) / 255.0
+        let blue = Double(rgb & 0xFF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue)
     }
 }
 
