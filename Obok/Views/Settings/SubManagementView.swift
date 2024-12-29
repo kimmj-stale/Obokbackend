00679e0548607fdd0530995aa1c8d32f51b9a6e3
min-gyoung <alsrud0205@naver.com>
2024-12-29 19:31:55 +0900

    subject management setting

--- Obok/Views/Settings/SubManagementView.swift
+++ Obok/Views/Settings/SubManagementView.swift
@@ -8,17 +8,37 @@
 import SwiftUI
 
 struct SubManagementView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var isNotificationOn: Bool = false
+    
     var body: some View {
-        VStack {
-            Text("과목 관리")
-                .font(.largeTitle)
-                .padding()
-            Spacer()
+        VStack(spacing: 20) {
+            HStack {
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss() // 이전 화면으로 돌아가기
+                }) {
+                    Image("back")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                }
+                Text("과목 관리")
+                    .font(.system(size: 16))
+                    .fontWeight(.bold)
+                    .padding(.leading)
+                Spacer()
+            }
+            
+            Text("과목을 숨기거나 다시 보이게 할 수 있어요.\n숨긴 과목은 메인과 모아보기에서 보이지 않아요.")
+                .font(.system(size: 18))
+                .fontWeight(.semibold)
+                .padding(.leading, -5)
         }
-        .navigationTitle("과목 관리") // 네비게이션 타이틀 설정
-        .navigationBarTitleDisplayMode(.inline) // 타이틀 인라인 표시
+        .padding()
+        .background(Color.white)
     }
 }
+
 
 struct SubManagementView_Previews: PreviewProvider {
     static var previews: some View {
