95a7c5556ef6ed8d60303e062d82c494a32e6473
min-gyoung <alsrud0205@naver.com>
2025-01-02 15:41:18 +0900

    current year

--- Obok/Views/Statistics/StatisticsView.swift
+++ Obok/Views/Statistics/StatisticsView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct StatisticsView: View {
     @Environment(\.presentationMode) var presentationMode
    
+    // 현재 연도 가져오기
+    private var currentYear: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy년"
+        return dateFormatter.string(from: Date())
+    }
     
     var body: some View {
         ZStack {
@@ -24,16 +30,23 @@ struct StatisticsView: View {
                             .scaledToFit()
                             .frame(width: 24, height: 24)
                     }
-
+                    
                     Spacer()
-
+                    
                     Text("통계")
                         .font(.system(size: 18))
                         .fontWeight(.bold)
                         .foregroundColor(.black)
                         .frame(maxWidth: .infinity, alignment: .leading)
-
-                    Spacer()
+                    
+                    Image("calendar")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 16, height: 16)
+                    
+                    Text(currentYear)
+                        .font(.system(size: 16))
+                        .fontWeight(.bold)
                 }
                 .padding([.top, .horizontal], 24)
             }
