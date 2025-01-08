4d4fa599e3ad82529fb2fdd40d0f67c273009c3f
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2025-01-09 01:26:57 +0900

    Add files via upload
    Signed-off-by: Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>

--- ObokBackEnd/Obok_Work_Session/src/main/java/DOIT/TeamPJT3/Obok/Controller/postController.java
+++ ObokBackEnd/Obok_Work_Session/src/main/java/DOIT/TeamPJT3/Obok/Controller/postController.java
@@ -2,43 +2,58 @@ package DOIT.TeamPJT3.Obok.Controller;
 
 import DOIT.TeamPJT3.Obok.Entity.Post;
 import DOIT.TeamPJT3.Obok.repository.PostRepository;
+import jakarta.persistence.PostUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/api/post")
+@CrossOrigin(origins = "http://localhost:3000")
 public class postController {
+
     @Autowired
-    //Di 셍성
     private PostRepository postRepository;
-    //get
+
+    // 모든 게시글 조회
     @GetMapping
-    public List<Post> getAll() {
+    public List<Post> getAllPosts() {
         return postRepository.findAll();
     }
 
-    @GetMapping("/{id}")
-    public Post getPostId(@PathVariable Long id) {
-        return postRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("contents not found"));
-    }
-
-    //update
+    // 게시글 생성
     @PostMapping
-    public Post updatePost(@PathVariable Long id , @RequestBody Post newpost) {
-        Post post = postRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("contents not found"));
-        post.setTitle(newpost.getTitle());
-        post.setContents(newpost.getContents());
-
+    public Post createPost(@RequestBody Post post) {
         return postRepository.save(post);
     }
 
-    //delete
-    @DeleteMapping
-    public void deletePost(@PathVariable Long id) {
-        postRepository.deleteById(id);
+    // 게시글 수정
+    @PutMapping("/{id}")
+    public Post updatePost(@PathVariable Long id , @RequestBody Post post) {
+        Post post1 = postRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 글을 불러올 수 없습니다."));
+        //저장할 데이터 타입들 전부 설정
+        post1.setSubject(post.getSubject());
+        post1.setPage(post.getPage());
+        post1.setPerC(post.getPerC());
+        post1.setMotion1(post.getMotion1());
+        post1.setMotion2(post.getMotion2());
+        post1.setCont1(post.getCont1());
+        post1.setCont2(post.getCont2());
+        post1.setCnt1(post.getCnt1());
+        post1.setCnt2(post.getCnt2());
+        post1.setCnt3(post.getCnt3());
+        return postRepository.save(post1);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 글을 불러올 수 없습니다."));
+        postRepository.delete(post);
+        return ResponseEntity.ok().build();
     }
 }
+
--- ObokBackEnd/Obok_Work_Session/src/main/java/DOIT/TeamPJT3/Obok/Entity/Post.java
+++ ObokBackEnd/Obok_Work_Session/src/main/java/DOIT/TeamPJT3/Obok/Entity/Post.java
@@ -1,39 +1,147 @@
 package DOIT.TeamPJT3.Obok.Entity;
 
-import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "post")
+@Table(name = "data")
+@Getter
+@Setter
 public class Post {
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //index
     @Id
-    private long Id;
-    @Column(nullable = false)
-    private String Title;
-    @Column(nullable = false , columnDefinition = "TEXT")
-    private String contents;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public Post() {}
+    //과목
+    @Column(nullable = false , name = "subject")
+    private Long subject;
 
-    //Lombok
-    public long getId() {
-        return Id;
-    }
-    public void setId(long id) {
-        this.Id = id;
-    }
-    public String getTitle() {
-        return Title;
-    }
-    public void setTitle(String title) {
-        Title = title;
-    }
-    public String getContents() {
-        return contents;
-    }
-    public void setContents(String contents) {
-        this.contents = contents;
+    //공부량
+    @Column(nullable = false , name = "page")
+    private Long page;
+
+    //이해도
+    @Column(nullable = false , name = "perC")
+    private Integer perC;
+
+    //마음가짐
+    @Column(nullable = false , name = "motion1")
+    private Integer motion1;
+
+    //마음가짐2
+    @Column(nullable = false , name = "motion2")
+    private Integer motion2;
+
+    //일기 내용
+    @Column(nullable = false, columnDefinition = "VARCHAR(255) DEFAULT 'No Content'")
+    private String cont1;
+
+    //아쉬웠던 점
+    @Column(nullable = false , columnDefinition = "VARCHAR(255) DEFAULT 'NO content'")
+    private String cont2;
+
+    //아쉬운 점
+    @Column(nullable = false , name = "cnt1")
+    private Integer cnt1;
+
+    //학습 능력
+    @Column(nullable = false , name = "cnt2")
+    private Integer cnt2;
+
+    //외부요인
+    @Column(nullable = false , name = "cnt3")
+    private Integer cnt3;
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
+    public void setSubject(Long subject) {
+        this.subject = subject;
+    }
+
+    public void setPage(Long page) {
+        this.page = page;
+    }
+
+    public void setPerC(Integer perC) {
+        this.perC = perC;
+    }
+
+    public void setMotion1(Integer motion1) {
+        this.motion1 = motion1;
+    }
+
+    public void setMotion2(Integer motion2) {
+        this.motion2 = motion2;
+    }
+
+    public void setCont1(String cont1) {
+        this.cont1 = cont1;
+    }
+
+    public void setCont2(String cont2) {
+        this.cont2 = cont2;
+    }
+
+    public void setCnt1(Integer cnt1) {
+        this.cnt1 = cnt1;
+    }
+
+    public void setCnt2(Integer cnt2) {
+        this.cnt2 = cnt2;
+    }
+
+    public void setCnt3(Integer cnt3) {
+        this.cnt3 = cnt3;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getSubject() {
+        return subject;
+    }
+
+    public Long getPage() {
+        return page;
+    }
+
+    public Integer getPerC() {
+        return perC;
+    }
+
+    public Integer getMotion1() {
+        return motion1;
+    }
+
+    public Integer getMotion2() {
+        return motion2;
+    }
+
+    public String getCont1() {
+        return cont1;
+    }
+
+    public String getCont2() {
+        return cont2;
+    }
+
+    public Integer getCnt1() {
+        return cnt1;
+    }
+
+    public Integer getCnt2() {
+        return cnt2;
+    }
+
+    public Integer getCnt3() {
+        return cnt3;
+    }
 }
+
